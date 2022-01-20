f364c8e8e958ca2bb2f7783b6c9e809030ba0827
luirusso <luirusso_@hotmail.it>
2022-01-20 16:42:31 +0100

    completed footer styling

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -5,21 +5,41 @@
 <footer>
     <div class="container d-flex justify-content-between py-5">
         <div>
-            <button class="d-flex align-items-center justify-content-center">
+            <button class="d-flex align-items-center justify-content-center fw-bold">
                 SIGN UP NOW!
             </button>
         </div>
 
-        <div class="d-flex">
-            <div>
+        <div class="follow-container d-flex">
+            <div class="follow fs-2 fw-bold">
                 FOLLOW US
             </div>
     
-            <div>
-                <ul>
+            <div class="socials-container">
+                <ul class="d-flex align-items-center">
                     <li>
                         <a href="">
-                            social
+                            <img src="{{ asset('img/footer-facebook.png') }}" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="{{ asset('img/footer-twitter.png') }}" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="{{ asset('img/footer-youtube.png') }}" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="{{ asset('img/footer-pinterest.png') }}" alt="">
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img src="{{ asset('img/footer-periscope.png') }}" alt="">
                         </a>
                     </li>
                 </ul>
